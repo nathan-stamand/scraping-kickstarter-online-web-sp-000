aad7f565e814434a4db29f31283e15d8cc59708e
Nathan St.Amand <nathansaint810@gmail.com>
2020-03-01 17:49:29 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,8 +6,7 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   projects = {} 
-
-  # projects: 
+  
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text 
     projects[title.to_sym] = {
@@ -18,11 +17,5 @@ def create_project_hash
     }
   end 
   projects
-  # title: kickstarter.css("h2.bbcard_name strong a").text
-  # image link: kickstarter.css("div.project-thumbnail a img").attribute("src").value
-  # description: kickstarter.css("p.bbcard_blurb").text
-  # location: kickstarter.css("ul.project-meta span.location-name").text
-  # percent funded: kickstarter.css("ul.project-stats li.first.funded strong").text.gsub("%", '')
 end
 
-create_project_hash
