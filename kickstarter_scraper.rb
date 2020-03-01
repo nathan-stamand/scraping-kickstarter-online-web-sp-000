1cace6156a2da39cad0748c7f84ed9108c08dded
Nathan St.Amand <nathansaint810@gmail.com>
2020-03-01 17:22:47 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,9 +5,8 @@ require "pry"
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  
+  binding.pry
   # projects: kickstarter.css("li.project.grid_4")
 end
 
 create_project_hash
-binding.pry
