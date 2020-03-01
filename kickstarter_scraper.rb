c303fad80963a0e2a00cda627c01003a0edb5d38
Nathan St.Amand <nathansaint810@gmail.com>
2020-03-01 17:48:08 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,7 +10,12 @@ def create_project_hash
   # projects: 
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text 
-    projects[title.to_sym] = {}
+    projects[title.to_sym] = {
+      :image_link => kickstarter.css("div.project-thumbnail a img").attribute("src").value,
+      :description => kickstarter.css("p.bbcard_blurb").text,
+      :location => kickstarter.css("ul.project-meta span.location-name").text
+      :percent_funded => kickstarter.css("ul.project-stats li.first.funded strong").text.gsub("%", '').to_i
+    }
   end 
   projects
   # title: kickstarter.css("h2.bbcard_name strong a").text
