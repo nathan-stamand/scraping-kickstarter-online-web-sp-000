55352465eca8c407fd55602da0ccb9fd314e9307
Nathan St.Amand <nathansaint810@gmail.com>
2020-03-01 17:40:52 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,8 +10,8 @@ def create_project_hash
   # title: kickstarter.css("h2.bbcard_name strong a").text
   # image link: kickstarter.css("div.project-thumbnail a img").attribute("src").value
   # description: kickstarter.css("p.bbcard_blurb").text
-  # location: kickstarter.css("span.location-name").text
-  # percent funded: kickstarter.css("ul.project-stats li.first.funded strong").text
+  # location: kickstarter.css("ul.project-meta span.location-name").text
+  # percent funded: kickstarter.css("ul.project-stats li.first.funded strong").text.gsub("%", '')
 end
 
 create_project_hash
